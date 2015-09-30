1a8a426a125b2afe368c84e215c239e9d2616191
s-creek <ogawa@oga-pc04.(none)>
2015-09-30 23:18:31 +0900

    first

---
+++ bin/setup_jvrc_tsml.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt-get install git cmake
+sudo apt-get install hrpsys-base python-omniorb omniidl-python
---
+++ script/test/guiinfo.py
@@ -0,0 +1,14 @@
+import os
+from user import *
+
+funcList = [
+  ["stop-rt-system", deactivateComps],
+  " ",
+  "seq play",
+  goInital,
+  goHalfSitting,
+  " ",
+  searchOn,
+  showQrDataList,
+  neck,
+]
