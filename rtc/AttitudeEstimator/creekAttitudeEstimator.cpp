e8b39123a1409c07cdd2c1477b69f8ce90c74bd4
grxuser <grxuser@cc07>
2016-11-04 22:18:40 +0900

    rename StateEstimator -> AttitudeEstimator

---
+++ rtc/AttitudeEstimator/creekAttitudeEstimator.cpp
@@ -0,0 +1,202 @@
+// 
+// reference url : http://tom.pycke.be/mav/71/kalman-filtering-of-imu-data
+//
+#include "creekAttitudeEstimator.h"
+using namespace creek;
+
+void calcRotFromRpy(Matrix3& out_R, double r, double p, double y)
+{
+    const double cr = cos(r), sr = sin(r), cp = cos(p), sp = sin(p), cy = cos(y), sy = sin(y);
+    out_R(0,0)= cp*cy;
+    out_R(0,1)= sr*sp*cy - cr*sy;
+    out_R(0,2)= cr*sp*cy + sr*sy;
+    out_R(1,0)= cp*sy;
+    out_R(1,1)= sr*sp*sy + cr*cy;
+    out_R(1,2)= cr*sp*sy - sr*cy;
+    out_R(2,0)= -sp;
+    out_R(2,1)= sr*cp;
+    out_R(2,2)= cr*cp;
+}
+
+
+// Module specification
+static const char* module_spec[] =
+  {
+    "implementation_id", "creekAttitudeEstimator",
+    "type_name",         "creekAttitudeEstimator",
+    "description",       "Sequence InPort component",
+    "version",           "1.0",
+    "vendor",            "s-creek",
+    "category",          "example",
+    "activity_type",     "SPORADIC",
+    "kind",              "SequenceInComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    ""
+  };
+
+
+creekAttitudeEstimator::creekAttitudeEstimator(RTC::Manager * manager)
+  : RTC::DataFlowComponentBase(manager),
+    m_accSensorIn("acc", m_accSensor),
+    m_gyroSensorIn("rate", m_gyroSensor),
+    m_accRefIn("accRef", m_accRef),
+    m_rpyOut("rpy", m_rpy)
+{
+}
+
+
+RTC::ReturnCode_t creekAttitudeEstimator::onInitialize()
+{
+  std::cout << "creekAttitudeEstimator : onInitialize" << std::endl;
+
+  //
+  // set port
+  //
+  addInPort("acc",     m_accSensorIn);
+  addInPort("rate",    m_gyroSensorIn);
+  addInPort("accRef",  m_accRefIn);
+
+  addOutPort("rpy",  m_rpyOut);
+
+  m_accSensor.data.length(3);
+  m_gyroSensor.data.length(3);
+  m_accRef.data.length(3);
+  //m_rpy.data.length(3);
+
+  for(int i=0; i<3; i++) {
+    m_accSensor.data[i]  = 0.0;
+    m_gyroSensor.data[i] = 0.0;
+    m_accRef.data[i]     = 0.0;
+    //m_rpy.data[i]        = 0.0;
+  }
+  RTC::Properties& prop = getProperties();
+  coil::vstring tmp = coil::split( prop["initBaseRpy"], "," );
+  std::vector<double> initrpy(tmp.size());
+  std::cout << "creekAttitudeEstimator :";
+  for( unsigned int i=0; i<tmp.size(); i++) {
+    coil::stringTo(initrpy[i], tmp[i].c_str());
+    std::cout << " [" << tmp[i] << ", " << initrpy[i] << "] ";
+  }
+  std::cout << std::endl;
+  m_rpy.data.r=initrpy[0];  m_rpy.data.p=initrpy[1];  m_rpy.data.y=initrpy[2];
+  //m_rpy.data.r=0.0;  m_rpy.data.p=0.0;  m_rpy.data.y=0.0;
+
+
+  //
+  // set default param
+  //
+  //RTC::Properties& prop = getProperties();
+  if ( ! coil::stringTo(m_dt, prop["dt"].c_str()) ) {
+    std::cerr << "creekAttitudeEstimator failed to prop[dt] " << prop["dt"] << "" << std::endl;
+    return RTC::RTC_ERROR;
+  }
+
+
+  for(int i=0; i<3; i++) {
+    m_kf[i].setP(0,0,0,0);
+    m_kf[i].setQ(0.001*m_dt, 0, 0, 0.003*m_dt);
+    //m_kf[i].setR(0.3);
+    m_kf[i].setR(0.03);
+    m_kf[i].setA(1, -m_dt, 0, 1);
+    m_kf[i].setB(m_dt, 0);
+    m_kf[i].setX(initrpy[i]);
+  }
+  
+
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t creekAttitudeEstimator::onActivated(RTC::UniqueId ec_id)
+{
+  std::cout << "creekAttitudeEstimator : onActivated" << std::endl;
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t creekAttitudeEstimator::onDeactivated(RTC::UniqueId ec_id)
+{
+  std::cout << "creekAttitudeEstimator : onDeactivated" << std::endl;
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t creekAttitudeEstimator::onExecute(RTC::UniqueId ec_id)
+{
+  if( m_accSensorIn.isNew() )   m_accSensorIn.read();
+  if( m_gyroSensorIn.isNew() )  m_gyroSensorIn.read();
+  
+  if( m_accRefIn.isNew() ) {
+    m_accRefIn.read();
+  }
+ 
+
+  //
+  // gravity = sensor - ref
+  //
+  Vector3 gacc;  // = ax, ay, az
+  for(int i=0; i<3; i++) {
+    gacc(i) = m_accSensor.data[i] - m_accRef.data[i];
+  }
+
+
+  //
+  // gyro (on world)
+  //
+  Matrix3 rot; //( rotFromRpy(m_rpy.data[0], m_rpy.data[1], m_rpy.data[2]) );
+  //calcRotFromRpy( rot, m_rpy.data[0], m_rpy.data[1], m_rpy.data[2] );
+  calcRotFromRpy( rot, m_rpy.data.r, m_rpy.data.p, m_rpy.data.y );
+  Vector3 tmp;  tmp << m_gyroSensor.data[0], m_gyroSensor.data[1], m_gyroSensor.data[2];
+  Vector3 gyro( rot * tmp );
+
+
+  //
+  // ax = -sin(p) * g
+  // ay = cos(p) * sin(r) * g
+  // az = cos(p) * cos(r) * g
+  //
+  Vector3 rpy;
+  rpy(1) = atan2(-gacc(0), sqrt(gacc(1)*gacc(1) + gacc(2)*gacc(2)));  // p : -90～90 [deg]  ->  cos(p) >= 0
+  rpy(0) = atan2(gacc(1), gacc(2));
+  //rpy(2) = m_rpy.data[2] + m_dt*gyro(2);
+  rpy(2) = m_rpy.data.y + m_dt*gyro(2);
+  
+
+  //
+  // update
+  //
+  //for(int i=0; i<3; i++)  m_rpy.data[i] = m_kf[i].filtering(rpy(i), m_gyroSensor.data[i]);
+  m_rpy.data.r = m_kf[0].filtering(rpy(0), m_gyroSensor.data[0]);
+  m_rpy.data.p = m_kf[1].filtering(rpy(1), m_gyroSensor.data[1]);
+  m_rpy.data.y = m_kf[2].filtering(rpy(2), m_gyroSensor.data[2]);
+
+  
+  /*
+  // debug
+  rpy << m_rpy.data.r, m_rpy.data.p, m_rpy.data.y;
+  Vector3 rpyDeg;
+  rpyDeg = rpy / M_PI * 180.0;
+  std::cout << "creekAttitudeEstimator : " << rpyDeg(0) << ", " << rpyDeg(1) << ", " << rpyDeg(2) << std::endl;
+  */  
+
+  m_rpyOut.write();
+  return RTC::RTC_OK;
+}
+
+
+//-----------------------------------------------------------------------
+
+
+extern "C"
+{
+  void creekAttitudeEstimatorInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(module_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<creekAttitudeEstimator>,
+                             RTC::Delete<creekAttitudeEstimator>);
+  }  
+};
---
+++ rtc/AttitudeEstimator/make.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+cd `dirname $0`
+
+#
+# user configuration
+#
+export COMP_NAME="creekAttitudeEstimator"
+
+
+#
+# not change !!
+#
+export RTM_ROOT=/usr
+
+#CMAKE_OPT="-DCMAKE_INSTALL_PREFIX:STRING=/usr/lib/choreonoid-1.5 -DCOMP_NAME=${COMP_NAME}"
+CMAKE_OPT="-DCMAKE_INSTALL_PREFIX:STRING=/opt/tsml/lib -DCOMP_NAME=${COMP_NAME}"
+MAKE_OPT="VERBOSE=1 $@"
+
+# for logging
+DATE=`/bin/date '+%Y%m%d%H%M'`
+mkdir -p log
+
+# cmake
+cmake . ${CMAKE_OPT} 2>&1 | tee log/build_${DATE}.log
+
+# make
+make ${MAKE_OPT} 2>&1 | tee -a log/build_${DATE}.log
+
+cd log
+ln -sfv build_${DATE}.log build.log
